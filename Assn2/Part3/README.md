2c1b6b0a3616cac24e0f0cfc22611876986ebf94
Shaelyn Tolkamp <tolkamps1@uvic.ca>
2020-02-28 11:40:01 -0800

    resource hierarchy solution to dining philosophers problem

--- Assn2/Part3/README.md
+++ Assn2/Part3/README.md
@@ -1,5 +1,9 @@
 Shaelyn Tolkamp
 V00875259
 
+### Solution
+I used a resource hierarchy solution to the dining philosophers problem. The hierarchy is determined by the index position of each fork in the sem fork array. Lower indexes are considered to be higher.
+
 ### Resources
-https://gist.github.com/davidjpfeiffer/2bda83fbba570592e4d46d5e47853d32
+I chose to implement the resource hierarchy solution from the wikipedia source linked in the assignment description: https://en.wikipedia.org/wiki/Dining_philosophers_problem. I also referenced this solution while writing my program and based most of the strucutre and set up off it: https://gist.github.com/davidjpfeiffer/2bda83fbba570592e4d46d5e47853d32.
+
--- Assn2/Part3/diningOut.c
+++ Assn2/Part3/diningOut.c
@@ -2,4 +2,112 @@
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
+#include <time.h>
+#include <unistd.h>
+
+
+#define PHILNUMBER 5
+
+
+typedef struct philosopher{
+    int position;
+    sem_t *leftFork;
+    sem_t *rightFork;
+} philosopher_t;
+
+
+
+/* Returns random number between max and min
+ * for determining sleep time
+ */
+int get_random_number(int max, int min)
+{
+  int result = (rand() % (max + 1));
+  if (result < min) result = min;
+  return result;
+}
+
+
+/**
+ * Get forks by locking semaphore if available
+ * Resource hierarchy is determined by the fork position in array with
+ * Zero index is at the top of the hierarchy IE. all philosophers take 
+ * their left fork first except the phil in position 4
+ */
+void get_forks(philosopher_t *philosopher){
+    // Phil 4 takes right fork first
+    if(philosopher->position == (PHILNUMBER-1)){
+        sem_wait(philosopher->rightFork);
+        sem_wait(philosopher->leftFork);
+    }
+    // All other Phils take left fork first
+    else{
+        sem_wait(philosopher->leftFork);
+        sem_wait(philosopher->rightFork);
+    }
+}
+
+
+/**
+ * Release forks by unlocking semaphore
+ */
+void release_forks(philosopher_t *philosopher){
+    sem_post(philosopher->rightFork);
+    sem_post(philosopher->leftFork);
+}
+
+
+
+/* Philosopher eats: sleep
+ */
+void eat(philosopher_t *philosopher){
+    // Sleep for a random amount of time
+    sleep(get_random_number(5,1));
+}
+
+
+/* Philosopher thinks: sleep
+ */
+void think(philosopher_t *philosopher){
+    // Sleep for a random amount of time
+    sleep(get_random_number(5,1));
+}
+
+
+void *philosopher_routine(void *arg){
+    philosopher_t *philosopher = (philosopher_t *)arg;
+    while(1){
+        think(philosopher);
+        get_forks(philosopher);
+        eat(philosopher);
+        release_forks(philosopher);
+    }
+
+}
+
+
+void start_philosophers(pthread_t *threads, sem_t *forks){
+    for(int k = 0; k < PHILNUMBER; k++){
+        philosopher_t* philosopher = malloc(sizeof(philosopher_t));
+        philosopher->position = k;
+        philosopher->leftFork = &forks[k];
+        philosopher->rightFork = &forks[(k+1) % PHILNUMBER];
+        pthread_create(&threads[k], NULL, philosopher_routine, (void *)philosopher);
+    }
+}
+
+
+/**
+ * Initialize threads and forks
+ */
+int main (){
+    sem_t forks[PHILNUMBER];
+    pthread_t threads[PHILNUMBER];
+    // Create forks
+    for (int k = 0; k < PHILNUMBER; k++){
+        sem_init(&forks[k], 0, 1);
+    }
+    // Create threads
+    start_philosophers(threads, forks);
+    pthread_exit(NULL);
+}
